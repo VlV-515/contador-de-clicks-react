7c7f715e142c79dd0ad69300060846589bc42af6
VlV <92279938+VlV-515@users.noreply.github.com>
2022-05-10 23:27:50 -0500

    Feat⚙️Crea la logica del contador <useState>

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
-import './App.css';
-import logo from './logo.svg';
+import { useState } from 'react'
+import './App.css'
+import logo from './logo.svg'
+import Boton from './components/Boton/Boton'
+import Contador from './components/Contador/Contador'
 
 function App() {
+  const [contador, setContador] = useState(0)
+
+  const manejarClic = () => {
+    setContador(contador + 1)
+  }
+  const resetContador = () => {
+    setContador(0)
+  }
+
   return (
-    <div className="App">
+    <div className='App'>
       <div className='logo-contenedor'>
-        <img
-        className='logo'
-        src={logo}
-        alt='Logo'
-        />
+        <img className='logo' src={logo} alt='Logo' />
       </div>
       <div className='contenedor-principal'>
-        
+        <Contador contador={contador} />
+        <Boton
+          texto='Sumar Clic'
+          esBotonClic={true}
+          manejarClic={manejarClic}
+        />
+        <Boton
+          texto='Reiniciar'
+          esBotonClic={false}
+          manejarClic={resetContador}
+        />
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
