9dd316c769903e2b56bad81312a1abd8d9de64a4
VlV <92279938+VlV-515@users.noreply.github.com>
2022-05-10 21:20:05 -0500

    Feat⚙️ Crea el logo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import logo from './logo.svg';
 function App() {
   return (
     <div className="App">
-      
+      <div className='logo-contenedor'>
+        <img
+        className='logo'
+        src={logo}
+        alt='Logo'
+        />
+      </div>
+      <div className='contenedor-principal'>
+        
+      </div>
     </div>
   );
 }
